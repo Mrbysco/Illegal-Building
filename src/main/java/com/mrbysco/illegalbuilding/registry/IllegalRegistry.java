5b3c94644c202261c68eaaa2f2fd7f50d7e318fd
Mrbysco <Mrbysco@users.noreply.github.com>
2023-04-08 22:06:56 +0200

    Update IllegalRegistry.java

--- src/main/java/com/mrbysco/illegalbuilding/registry/IllegalRegistry.java
+++ src/main/java/com/mrbysco/illegalbuilding/registry/IllegalRegistry.java
@@ -15,9 +15,9 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
-import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class IllegalRegistry {
 	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Reference.MOD_ID);
